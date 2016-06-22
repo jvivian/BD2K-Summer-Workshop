584e014c22ee103142dc0bf224c7c5f7b2b95d10
Jacob Pfeil <jpfeil@ucsc.edu>
2016-06-21 20:29:36 -0700

    Remove yes command from install

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Run this to install necessary software for the workshop
 
-sudo apt-get update
-sudo apt-get install samtools bwa git htop
-wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh
-bash yes | Anaconda2-4.0.0-Linux-x86_64.sh
-rm Anaconda2-4.0.0-Linux-x86_64.sh
-source ~/.bashrc
+sudo apt-get -y update
+sudo apt-get -y install samtools bwa git htop
+wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
+rm ~/anaconda.sh
+export PATH="$HOME/anaconda/bin:$PATH"
 conda install seaborn
 
