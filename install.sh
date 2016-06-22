bd420d5a344541bca1f418086899bfafb333bf56
Jacob Pfeil <jpfeil@ucsc.edu>
2016-06-21 18:37:36 -0700

    Start git section

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install samtools bwa git
+sudo apt-get install samtools bwa git htop
 wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh
 bash Anaconda2-4.0.0-Linux-x86_64.sh
 rm Anaconda2-4.0.0-Linux-x86_64.sh
