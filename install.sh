0386a89999914388d4b2a04d1cb335feef987ad8
Jacob Pfeil <jpfeil@ucsc.edu>
2016-06-21 20:33:27 -0700

    Use conda -y flag

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Run this to install necessary software for the workshop
-
 sudo apt-get -y update
 sudo apt-get -y install samtools bwa git htop
 wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh -O ~/anaconda.sh
 bash ~/anaconda.sh -b -p $HOME/anaconda
 rm ~/anaconda.sh
 export PATH="$HOME/anaconda/bin:$PATH"
-conda install seaborn
+conda install -y seaborn
 
