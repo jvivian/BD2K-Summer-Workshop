bccf0dc6ef098a777d5cd8215abb084723992cfd
Jacob Pfeil <jpfeil@ucsc.edu>
2016-06-21 20:21:57 -0700

    Add yes command to install file

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Run this to install necessary software for the workshop
+
 sudo apt-get update
 sudo apt-get install samtools bwa git htop
 wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh
-bash Anaconda2-4.0.0-Linux-x86_64.sh
+bash yes | Anaconda2-4.0.0-Linux-x86_64.sh
 rm Anaconda2-4.0.0-Linux-x86_64.sh
+source ~/.bashrc
+conda install seaborn
+
