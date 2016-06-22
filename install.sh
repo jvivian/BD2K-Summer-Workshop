dbe6d1e19a89ace64c72d1d640a4f189be8354c0
Jacob Pfeil <jpfeil@ucsc.edu>
2016-06-21 17:20:19 -0700

    Add install bash script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install samtools bwa git
+wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh
+bash Anaconda2-4.0.0-Linux-x86_64.sh
+rm Anaconda2-4.0.0-Linux-x86_64.sh
